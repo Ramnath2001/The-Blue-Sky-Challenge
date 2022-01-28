18148df80b2c9eaf97a7cca7a9251a97e3f39e60
Ramnath K <ramnath0912@gmail.com>
2022-01-28 14:00:48 +0530

    fixed csv problem

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require("express")
 const bodyParser = require("body-parser")
-const csv = require('csv-parser')
-const fs = require('fs')
-
+const fs = require('fs');
 
 
 const app = express();
@@ -11,13 +9,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 const results = []
-fs.createReadStream('\public\\finalResults.csv').pipe(csv({})).on('data', (data) => results.push(data)).on('end', ()=>{
-    // console.log(results);
-    // console.log(results[0]['CO (Actual)']);
-})
-
 
 app.get("/", function(req, res){
+    let rawdata = fs.readFileSync('./public/data.json');
+    let results = JSON.parse(rawdata);
     res.render('home', {results: results});
 })
 
